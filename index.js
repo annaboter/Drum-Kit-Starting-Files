62e611f0f0dc5242b891c74a1f33e50dc850e60f
Anna Bothe <annabothe0@gmail.com>
2024-05-31 14:18:18 +0200

    refactor js code

--- index.js
+++ index.js
@@ -4,14 +4,24 @@
 // })
 
 var buttons = document.querySelectorAll(".drum");
-var tom1 = new Audio('sounds/tom-1.mp3');
-var tom2 = new Audio('sounds/tom-2.mp3');
-var tom3 = new Audio('sounds/tom-3.mp3');
-var tom4 = new Audio('sounds/tom-4.mp3');
-var snare = new Audio('sounds/snare.mp3');
-var crash = new Audio('sounds/crash.mp3');
-var kick = new Audio('sounds/kick-bass.mp3');
+var sounds = [
+  new Audio("sounds/tom-1.mp3"),
+  new Audio("sounds/tom-2.mp3"),
+  new Audio("sounds/tom-3.mp3"),
+  new Audio("sounds/tom-4.mp3"),
+  new Audio("sounds/snare.mp3"),
+  new Audio("sounds/crash.mp3"),
+  new Audio("sounds/kick-bass.mp3")
+];
 
+// refactored code
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    sounds[i].play();
+  });
+}
+
+// first code
 // buttons.forEach(function(button) {
 //   button.addEventListener("click", function() {
 //     var audio = new Audio('sounds/tom-1.mp3');
@@ -19,24 +29,24 @@ var kick = new Audio('sounds/kick-bass.mp3');
 //   });
 // });
 
-buttons[0].addEventListener("click", function() {
-  tom1.play();
-});
-buttons[1].addEventListener("click", function() {
-  tom2.play();
-});
-buttons[2].addEventListener("click", function() {
-  tom3.play();
-});
-buttons[3].addEventListener("click", function() {
-  tom4.play();
-});
-buttons[4].addEventListener("click", function() {
-  snare.play();
-});
-buttons[5].addEventListener("click", function() {
-  crash.play();
-});
-buttons[6].addEventListener("click", function() {
-  kick.play();
-});
+// buttons[0].addEventListener("click", function () {
+//   tom1.play();
+// });
+// buttons[1].addEventListener("click", function () {
+//   tom2.play();
+// });
+// buttons[2].addEventListener("click", function () {
+//   tom3.play();
+// });
+// buttons[3].addEventListener("click", function () {
+//   tom4.play();
+// });
+// buttons[4].addEventListener("click", function () {
+//   snare.play();
+// });
+// buttons[5].addEventListener("click", function () {
+//   crash.play();
+// });
+// buttons[6].addEventListener("click", function () {
+//   kick.play();
+// });
